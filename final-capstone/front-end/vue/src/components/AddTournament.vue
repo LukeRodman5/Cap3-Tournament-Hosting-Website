b51ee77133f55866affef89b66579e4f694c9357
Anne Hoback <ahoback76@gmail.com>
2021-04-06 08:07:36 -0400

    tues 807am

--- final-capstone/front-end/vue/src/components/AddTournament.vue
+++ final-capstone/front-end/vue/src/components/AddTournament.vue
@@ -1,9 +1,26 @@
 <template>
-    <form>
-        <!-- <div class "form-element">
-            <label for ="name"> Name</label>
-            <textarea id="name">Please enter the name of your tournament</textarea>
-        </div> -->
+    <form> <!--v-on: submit.prevent="addTournament"-->
+     <div class="form-element">
+        <label for="name">Tournament Name:</label>
+        <input id="name" type="text" v-model="newTournament.name" />
+    </div>
+    <div class="form-element">
+        <label for="description"> Tournament Description:</label>
+        <input id="name" type="text" v-model="newTournament.description"/>
+    </div>
+    <div class = "form-element">
+        <label for ="descrption"> Tournament Host:</label>
+        <input id="host" type="text" v-model="newTournament.host"/> 
+        <!-- would be nice to have this field prefilled and non-editable with the logged in uers's name. This will allow for matching in the database.-->
+    </div>
+    <div class ="form-element">
+        <label for ="start-date"> Start Date:</label>
+        <input id="start-date" type="date" v-model="newTournament.startDate"/>
+    </div> 
+    <div class ="form-element">
+        <label for ="end-date"> End Date:</label>
+        <input id="start-date" type="date" v-model="newTournament.endDate"/>
+    </div> 
     </form>
 </template>
 
