f312906a1f678a551398396cba57257487af8465
Luke Rodman <luke.rodman5@gmail.com>
2021-04-06 12:08:31 -0400

    matches pojos & getters setters pushed

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/MatchesDAO.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/MatchesDAO.java
@@ -1,5 +1,18 @@
 package com.techelevator.application.dao;
 
-public class MatchesDAO {
+import java.util.List;
 
+import com.techelevator.application.model.Matches;
+
+public interface MatchesDAO {
+
+	List<Matches> getAllMatches();
+	List<Matches> getMatchById();
+	Matches createAMatch();
+	Matches deleteAMatch();
+	List<Matches> getMatchesByDate();
+	String updateAMatchId();
+	Long updateStartDate();
+	Long updateStartTime();
+		
 }
