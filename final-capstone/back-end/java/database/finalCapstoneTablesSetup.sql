6b0d9d6119eb1e806e14d2ca92b095b74084ea64
Louis Dinh <louisd895@gmail.com>
2021-04-06 08:37:26 -0700

    Added relator table between users and matches

--- final-capstone/back-end/java/database/finalCapstoneTablesSetup.sql
+++ final-capstone/back-end/java/database/finalCapstoneTablesSetup.sql
@@ -42,6 +42,15 @@ CREATE TABLE tournaments_matches
     )
 ;
 
+CREATE TABLE users_matches
+    (
+    user_id INTEGER
+    , match_id INTEGER
+    , CONSTRAINT pk_users_matches_user_id_match_id
+        PRIMARY KEY (user_id, match_id)
+    )
+;
+
 -- Setting foreign keys
 ALTER TABLE tournaments
     ADD CONSTRAINT fk_users_user_id
@@ -60,30 +69,15 @@ ALTER TABLE tournaments_matches
     FOREIGN KEY (match_id)
     REFERENCES matches(match_id)
 ;
-=======
-    , particpant_num    INTEGER
-    , CONSTRAINT pk_tournaments_tourney_id 
-        PRIMARY KEY (tourney_id)
-    , CONSTRAINT fk_users_user_id          
-        FOREIGN KEY (tourney_host) REFERENCES users(user_id));
-        
-CREATE TABLE matches
-     ( match_id         SERIAL
-     , competitor_one   CHARACTER VARYING(20)
-     , competitor_two   CHARACTER VARYING(20)
-     , start_time       TIME
-     , start_date       DATE
-     , CONSTRAINT pk_matches_match_id
-         PRIMARY KEY (match_id)
-     , CONSTRAINT fk_tournaments_start_date 
-         FOREIGN KEY (start_date) REFERENCES tournaments(start_date));    
-     
-     
-     
-     
-    -- , CONSTRAINT Fk_tournaments_tourney_id FOREIGN KEY (start_date) REFERENCES tournaments(start_date));
 
-    --determine extras "nice to haves" vs requirements of projects
-    --need location integation for google maps finding tournament
-    --, team_rank **needs assigned to a table for ranking
->>>>>>> 7e3e0e2b0f168621054e3738ba26b18179691679
+ALTER TABLE users_matches
+    ADD CONSTRAINT fk_users_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users(user_id)
+;
+
+ALTER TABLE users_matches
+    ADD CONSTRAINT fk_matches_match_id
+    FOREIGN KEY (match_id)
+    REFERENCES matches(match_id)
+;
