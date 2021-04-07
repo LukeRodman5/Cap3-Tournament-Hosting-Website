2c64bb7635473903b87b1ade390f6bae2891ec22
Louis Dinh <louisd895@gmail.com>
2021-04-07 06:55:11 -0700

    Changed vue active name and minor spacing on api controller

--- final-capstone/front-end/vue/src/components/AddTournament.vue
+++ final-capstone/front-end/vue/src/components/AddTournament.vue
@@ -43,7 +43,7 @@ export default {
                 tourneyHost: this.$store.state.user.username,
                 startDate:'',
                 endDate:'',
-                isActive:true,
+                active:true,
                 openForReg:true,
                 maxNumOfParticipants: 20,
                 numOfParticipants:0
@@ -54,26 +54,26 @@ export default {
     methods:{
       addTournament(){
             
-             applicationServices.addTournament(this.newTournament).then(response =>{
+        applicationServices.addTournament(this.newTournament).then(response =>{
              if(response.status === 201){
-                     // this.getTournaments()
-                    this.newTournament={
-                        tourneyName: '',
-                        tourneyDesc:'',
-                        tourneyHost: this.$store.state.user.username,
-                        startDate:'',
-                        endDate:'',
-                        isActive:true,
-                        openForReg:true,
-                        maxNumOfParticipants: 20,
-                        numOfParticipants:0
-            }
+                // this.getTournaments()
+                this.newTournament={
+                    tourneyName: '',
+                    tourneyDesc:'',
+                    tourneyHost: this.$store.state.user.username,
+                    startDate:'',
+                    endDate:'',
+                    active:true,
+                    openForReg:true,
+                    maxNumOfParticipants: 20,
+                    numOfParticipants:0
+                }
                  this.$router.push("/")
-                 }//end of if 
-             })//end then
-         },//end add tournament
+            }//end of if 
+        })//end then
+    },//end add tournament
         
-    }
+}   
 
 
 }
