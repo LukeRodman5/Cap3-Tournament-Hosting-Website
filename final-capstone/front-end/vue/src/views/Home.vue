eba0d88c410712fab7462e79e28136db15de3a8a
Michael Phanhthalath <mphanhthalath@gmail.com>
2021-04-07 11:22:41 -0400

    wed1122

--- final-capstone/front-end/vue/src/views/Home.vue
+++ final-capstone/front-end/vue/src/views/Home.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <h1>Tournament Manager</h1>
     <p>Your home for hosting, finding and playing tournaments of all kinds</p>
+    <browse/>
   </div>
 </template>
 
 <script>
+import Browse from '@/components/Browse.vue'
 export default {
-  name: "home"
+  name: "home",
+  components: {
+  Browse
+  }
+ 
 };
 </script>
