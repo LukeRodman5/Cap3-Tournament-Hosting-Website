2892a22701effc5547d245201c59bfcae0a061c8
Anne Hoback <ahoback76@gmail.com>
2021-04-08 15:21:20 -0400

    1521 thurs

--- final-capstone/front-end/vue/src/App.vue
+++ final-capstone/front-end/vue/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link> | 
       <router-link v-bind:to="{ name: 'add-tournament' }">Create a Tournament</router-link> | 
+      <router-link v-bind:to="{ name: 'my-tournaments' }">Show My Tournaments</router-link> |
       <router-link v-bind:to="{ name: 'browse'}">Browse</router-link> | 
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{ name: 'login' }" v-else>Login</router-link>
