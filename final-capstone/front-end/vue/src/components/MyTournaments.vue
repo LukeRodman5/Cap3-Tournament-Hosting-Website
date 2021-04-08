0140123b9fa41b54417e1dfd30e7c49f9a96bc1d
Anne Hoback <ahoback76@gmail.com>
2021-04-08 16:16:02 -0400

    thurs 1615

--- final-capstone/front-end/vue/src/components/MyTournaments.vue
+++ final-capstone/front-end/vue/src/components/MyTournaments.vue
@@ -1,7 +1,39 @@
 <template>
 <div>
-    display a list of tournaments where I am the host
-    display a list of tournaments where I am participant
+    <!--display a list of tournaments where I am the host-->
+    <table class = "host">
+        <thead>
+            <h2> Tournaments I'm Hosting</h2>
+        </thead>
+      <tbody id="tournament host">
+            <tr v-for="tournament in this.$store.state.tournaments" :key="tournament.tourneyId">
+              <td class="name">{{tournament.tourneyName}}</td>
+              <td class="description">{{tournament.tourneyDesc}}</td>
+              <td class="start-date">{{tournament.startDate}}</td>
+              <td class="end-date">{{tournament.endDate}}</td>
+              <td class="participants">{{tournament.numOfParticipants}}</td>
+              <td class="max-participants">{{tournament.maxNumOfParticipants}}</td>
+              <router-link v-bind:to="{name: 'tournament-manage', params: {tourneyID: tournament.tourneyId}}" tag="input" type="button" value="Manage Tournament"></router-link>
+            </tr>
+      </tbody>
+    </table>
+    <!--display a list of tournaments where I am participant-->
+    <table class = "play">
+        <thead>
+            <h2> Tournaments I'm Playing</h2>
+        </thead>
+      <tbody id="tournament play">
+            <tr v-for="tournament in this.$store.state.tournaments" :key="tournament.tourneyId">
+              <td class="name">{{tournament.tourneyName}}</td>
+              <td class="description">{{tournament.tourneyDesc}}</td>
+              <td class="start-date">{{tournament.startDate}}</td>
+              <td class="end-date">{{tournament.endDate}}</td>
+              <td class="participants">{{tournament.numOfParticipants}}</td>
+              <td class="max-participants">{{tournament.maxNumOfParticipants}}</td>
+              <button class="btnLeaveTourney" v-on:click="leaveTourney(currentTournament.tourneyId, $store.state.user.username)">Leave Tournament</button>
+            </tr>
+      </tbody>
+    </table>
 </div>
 </template>
 
---
+++ final-capstone/front-end/vue/src/views/MyTournaments.vue
@@ -0,0 +1,19 @@
+ <template>
+    <div>
+        <h1>My Tournaments</h1>
+        <my-tournaments/>
+    </div>
+</template>
+ 
+ <script>
+import MyTournaments from '../components/MyTournaments.vue'
+export default {
+  components: { 
+    MyTournaments
+    } 
+}
+</script>
+ 
+ <style>
+ 
+ </style>
