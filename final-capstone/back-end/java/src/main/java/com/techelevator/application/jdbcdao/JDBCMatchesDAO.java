9e1c6677b14d323e75e6c528c88eca17b944efb1
Hector Perez <hjoelperez216@gmail.com>
2021-04-12 13:04:21 -0400

    updated users_matches

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/jdbcdao/JDBCMatchesDAO.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/jdbcdao/JDBCMatchesDAO.java
@@ -17,7 +17,7 @@ public class JDBCMatchesDAO implements MatchesDAO {
 		 	this.jdbcTemplate = jdbcTemplate;}
 
 	@Override
-	 public List<Matches> getAllMatches() { 
+	 public List<Matches> getAllMatches() { // update to by tourneyID
 			List<Matches>listOfMatches = new ArrayList<>();
 			String sql = "select * from matches";
 			SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
@@ -40,12 +40,12 @@ public class JDBCMatchesDAO implements MatchesDAO {
 
 	@Override
 	 public void createAMatch(LocalDate startDate, LocalDate startTime) { // needs specified
-			String sql = "insert into matches (start_date, start_time) values (?, ?)";
+			String sql = "insert into matches (start_time, start_date) values (?, ?)";
 			jdbcTemplate.update(sql, startDate, startTime);}
 		
 	@Override
 	 public void updateAMatch(LocalDate startDate, LocalDate startTime, int matchId) {
-			String sql = "update matches set where match_id = ?, start_date = ?, start_time = ?";
+			String sql = "update matches set where match_id = ?, start_time = ?, start_date = ?";
 			jdbcTemplate.update(sql, startDate, startTime, matchId);}
 	
 	@Override
@@ -55,13 +55,35 @@ public class JDBCMatchesDAO implements MatchesDAO {
 	
 	@Override
 	 public void addUserToMatch(String username, int matchId) {
-			String sql = "insert into users_matches (user_id, match_id) values ((select user_id from users where username = ?), ?)";
+			String sql = "insert into users_matches (user_id, match_id, win_status) values ((select user_id from users where username = ?), ?, null)";
 			jdbcTemplate.update(sql, username, matchId);}
 	
 	@Override
 	 public void updateUserToDiffMatch(int newMatchId, String username, int currentMatchId) {
 			String sql = "update users_matches set match_id = ? where user_id = (select user_id from users where username = ?) and match_id = ?";
 			jdbcTemplate.update(sql, newMatchId, username, currentMatchId);}
+	
+	@Override
+	public void removeUserFromMatch(String username, long matchID) {
+			String sql = "delete from users_matches where user_id = (select user_id from users where username = ?) and match_id = ?";
+			jdbcTemplate.update(sql, username, matchID);
+		
+	}
+
+	
+	@Override
+	public void addMatchToTourney(long tourneyID, long matchID) {
+			String sql = "insert into tournaments_matches (tourney_id, match_id) values (?,?)";
+			jdbcTemplate.update(sql, tourneyID, matchID);
+		
+	}
+	
+	@Override
+	public void removeMatchFromTourney(long tourneyID, long matchID) {
+			String sql = "delete from tournaments_matches where tourney_id = ? and match_id = ?";
+			jdbcTemplate.update(sql, tourneyID, matchID);
+		
+	}
 
 	@Override
 	 public List<Matches> getMatchesByDate() {
@@ -89,4 +111,9 @@ public class JDBCMatchesDAO implements MatchesDAO {
 		matchRow.setStartDate(results.getDate("start_date").toLocalDate());
 		matchRow.setStartTime(results.getDate("start_time").toLocalDate());
 		return matchRow;}
+
+	
+	
+
+	
 }
