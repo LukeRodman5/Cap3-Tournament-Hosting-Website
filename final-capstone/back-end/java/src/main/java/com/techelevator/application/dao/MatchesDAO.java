83b4c58fe52a9e0cbd12c7b28d4d74d913ebc405
Luke Rodman <luke.rodman5@gmail.com>
2021-04-12 10:27:13 -0400

    pulling current update

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/MatchesDAO.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/MatchesDAO.java
@@ -2,21 +2,20 @@ package com.techelevator.application.dao;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import com.techelevator.application.model.Matches;
 
-public interface MatchesDAO { 		  // update match 
+public interface MatchesDAO {// update match 
 
-	List<Matches> getAllMatches();
-	List<Matches> getMatchById(int matchId);
-	void createAMatch(LocalDate startDate, LocalDate startTime); 
-	void deleteAMatch(int matchId);   // keep
-	void updateAMatch(LocalDate startDate, LocalDate startTime, int matchId);
-	void addUserToMatch(String username, int matchId);
-	void updateUserToDiffMatch(int newMatchId, String username, int currentMatchId);
-	List<Matches> getMatchesByDate(); // No
-	String updateAMatchId();          // Not this time
-	Long updateStartDate();           // Changed to update an entire match instead
-	Long updateStartTime();           // Changed to update an entire match instead
+	List<Matches> getAllMatches();//
+	List<Matches> getMatchById(int matchId);//
+	List<Matches> getMatchesByDate();// No
+	void createAMatch(LocalDate startDate, LocalDate startTime);//
+	void updateAMatch(LocalDate startDate, LocalDate startTime, int matchId);//
+	void deleteAMatch(int matchId);// Keep
+	void addUserToMatch(String username, int matchId);//
+	void updateUserToDiffMatch(int newMatchId, String username, int currentMatchId);//
+	String updateAMatchId();// Not this time
+	Long updateStartDate();// Changed to update an entire match instead
+	Long updateStartTime();// Changed to update an entire match instead
 	
 	}
